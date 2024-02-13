cc0e74d7bd305d03821ba2ccbb0fc77ec2db0791
Glebberr <gleb555444@gmail.com>
2024-02-14 01:12:56 +0200

    add redux toolkit

---
+++ client/src/redux/user/userSlice.js
@@ -0,0 +1,33 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+// setting up a Redux slice for managing user authentication state using the Redux Toolkit library.
+
+const initialState = {
+    currentUser: null,
+    error: null,
+    loading: false,
+};
+
+const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {
+        signInStart: (state) => {
+            state.loading = true;
+        },
+        signInSuccess: (state, action) => {
+            state.currentUser = action.payload;
+            state.loading = false;
+            state.error = null;
+        },
+        signInFailure: (state, action) => {
+            state.error = action.payload;
+            state.loading = false;
+        }
+    }
+});
+
+export const { signInStart, signInSuccess, signInFailure } = userSlice.actions;
+
+export default userSlice.reducer;
