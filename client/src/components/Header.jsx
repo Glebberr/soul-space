f7d1cb267ed553fa621097217bc9dde3c7ea49b1
Glebberr <gleb555444@gmail.com>
2024-02-14 04:10:52 +0200

    update the header and make the profile page private

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,9 @@
-import { FaSearch } from 'react-icons/fa'
-import { Link } from 'react-router-dom'
+import { FaSearch } from 'react-icons/fa';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
+    const { currentUser } = useSelector(state => state.user)
     return (
         <header className='bg-slate-200 shadow-md'>
 
@@ -12,25 +14,36 @@ export default function Header() {
                         <span className='text-slate-700'>Space</span>
                     </h1>
                 </Link>
+
                 <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
                     <input type="text" placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64' />
                     <FaSearch className='text-slate-600' />
                 </form>
+
                 <ul className='flex gap-4'>
 
                     <Link to='/'>
-                        <li className='hidden sm:inline text-slate-700 hover:underline'>Home</li>
+                        <li className='hidden sm:inline text-slate-700 hover:underline'>
+                            Home
+                        </li>
                     </Link>
 
                     <Link to='/about'>
-                        <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
+                        <li className='hidden sm:inline text-slate-700 hover:underline'>
+                            About
+                        </li>
                     </Link>
 
-                    <Link to='/sign-in'>
-                        <li className='text-slate-700 hover:underline'>Sign in</li>
+                    <Link to='/profile'>
+                        {currentUser ? (
+                            <img className='rounded-full h-7 w-7 object-cover' src={currentUser.avatar} alt="" />
+                        ) : (
+                            <li className='text-slate-700 hover:underline'>Sign in</li>
+                        )}
+
                     </Link>
                 </ul>
             </div>
-        </header>
+        </header >
     )
 }
